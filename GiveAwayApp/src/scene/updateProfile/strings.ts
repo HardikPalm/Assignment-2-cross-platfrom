ee99c9742ffb058ca788760be06820acdb6a38f7
Hardik Palm <124969793+HardikPalm@users.noreply.github.com>
2024-12-13 10:43:39 +0530

    Delete GiveAwayApp directory

--- GiveAwayApp/src/scene/updateProfile/strings.ts
+++
@@ -1,9 +0,0 @@
-export const Strings = {
-    'title': 'Welcome to app',
-    'firstName': 'First Name*',
-    'lastName': 'Last Name*',
-    'email': 'Email Address*',
-    'password': 'Password*',
-    'refer': 'Referral Code(Optional)',
-    'login': 'Register',
-}
--- GiveAwayApp/src/utils/urls/url.ts
+++
@@ -1,8 +0,0 @@
-let environment = 1;
-
-export let Url =
-  environment == 1
-    ? "http://10.0.0.238:3011/api"
-    : "";
-
-export const AuthUrl = `${Url}/auth/v1`;
